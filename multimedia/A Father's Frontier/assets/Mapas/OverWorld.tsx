afaf702b02e50dbec59ee66f6e78581610b5d94d
RubenMoyaL <107984125+RubenMoyaL@users.noreply.github.com>
2023-04-18 00:03:36 +0200

    Revisión diaria - 17/04/2023
    - A FATHER'S FRONTIER -
    
    La versión actual es jugable y se encuentra en un estado relativamente aceptable.
    
    FEATURES:
    - Tileset de exterior actualizado.
    - Creado mapa de Londres (OW)

---
+++ multimedia/A Father's Frontier/assets/Mapas/OverWorld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="OverWorld" tilewidth="32" tileheight="32" tilecount="592" columns="8">
+ <image source="../Tilesets/tileset.exterior.png" width="256" height="2368"/>
+</tileset>
