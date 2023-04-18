570c8a9e67520ac38d65f0a824dcd51f390e5ed7
RubenMoyaL <107984125+RubenMoyaL@users.noreply.github.com>
2023-04-18 22:20:36 +0200

    Revisión diaria - 18/04/2023
    - A FATHER'S FRONTIER -
    
    La versión actual es jugable y se encuentra en un estado relativamente aceptable.
    
    FEATURES:
    - Tileset de interior actualizado.
    - Creado un mapa esquematizado de la ciudad para la orientación del jugador.
    - Creada la casa del protagonista (OW)
    - Creado el supermercado (OW)

---
+++ multimedia/A Father's Frontier/assets/Mapas/OverWorld.Interiores.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="OverWorld.Interiores" tilewidth="32" tileheight="32" tilecount="320" columns="16">
+ <image source="../Tilesets/tileset.interior.png" width="512" height="640"/>
+</tileset>
